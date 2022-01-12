afcd268e0da7a14c12a86aa962cee4e441829631
d_jsrsc <d_jsrsc@163.com>
2022-01-12 20:50:43 +0800

    sort algorithem left

--- src/sort/mod.rs
+++ src/sort/mod.rs
@@ -4,11 +4,23 @@ mod cocktail_shaker_sort;
 mod comb_sort;
 mod counting_sort;
 mod gnome_sort;
+mod heap_sort;
+mod insertion_sort;
+mod merge_sort;
+mod odd_even_sort;
+mod quick_sort;
+mod radix_sort;
+mod selection_sort;
+mod shell_sort;
+mod stooge_sort;
 
 pub use bubble_sort::bubble_sort;
 
 #[allow(unused)]
 pub fn is_sorted<T: Ord>(ve2: &[T]) -> bool {
+    if ve2.is_empty() {
+        return true;
+    }
     let mut sorted = true;
     for i in 0..ve2.len() - 1 {
         if ve2[i] > ve2[i + 1] {
---
+++ src/sort/quick_sort.rs
@@ -0,0 +1,79 @@
+use std::cmp::PartialOrd;
+
+#[allow(unused)]
+pub fn quick_sort<T: Ord>(arr: &mut [T]) {
+    let len = arr.len();
+    _quick_sort(arr, 0, (len - 1) as isize)
+}
+
+fn _quick_sort<T: Ord>(arr: &mut [T], lo: isize, hi: isize) {
+    if lo < hi {
+        let p = partition(arr, lo, hi);
+        _quick_sort(arr, lo, p - 1);
+        _quick_sort(arr, p + 1, hi);
+    }
+}
+
+fn partition<T: PartialOrd>(arr: &mut [T], lo: isize, hi: isize) -> isize {
+    let pivot = hi as usize;
+    let mut i = lo - 1;
+    let mut j = hi;
+
+    loop {
+        i += 1;
+        while arr[i as usize] < arr[pivot] {
+            i += 1;
+        }
+        j -= 1;
+        while j >= 0 && arr[j as usize] > arr[pivot] {
+            j -= 1;
+        }
+        if i >= j {
+            break;
+        } else {
+            arr.swap(i as usize, j as usize);
+        }
+    }
+    arr.swap(i as usize, pivot as usize);
+    i
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn single_element() {
+        let mut arr = vec![1];
+        quick_sort(&mut arr);
+        assert_eq!(&arr, &[1]);
+    }
+
+    #[test]
+    fn sorted_array() {
+        let mut arr = vec![1, 2, 3, 4];
+        quick_sort(&mut arr);
+        assert_eq!(&arr, &[1, 2, 3, 4]);
+    }
+
+    #[test]
+    fn unsorted_array() {
+        let mut arr = vec![3, 4, 2, 1];
+        quick_sort(&mut arr);
+        assert_eq!(&arr, &[1, 2, 3, 4]);
+    }
+
+    #[test]
+    fn odd_number_of_elements() {
+        let mut arr = vec![3, 4, 2, 1, 7];
+        quick_sort(&mut arr);
+        assert_eq!(&arr, &[1, 2, 3, 4, 7]);
+    }
+
+    #[test]
+    fn repeated_elements() {
+        let mut arr = vec![542, 542, 542, 542];
+        quick_sort(&mut arr);
+        assert_eq!(&arr, &vec![542, 542, 542, 542]);
+    }
+}
